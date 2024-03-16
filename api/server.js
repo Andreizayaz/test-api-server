2e9ce7698dcc3c62ed081a5ba7446b2e34f54adb
Andreizayaz <poznavauka@mail.ru>
2024-03-16 21:25:20 +0300

    [feat]: create json-server for vercel

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 // See https://github.com/typicode/json-server#module
+const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("/tmp/db.json");
+const router = jsonServer.router(path.join("tmp", "data.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
